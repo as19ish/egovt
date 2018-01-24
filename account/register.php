5500c33ba048085ad148082ec8f10ef8bace8f4e
arrow <as19ish@gmail.com>
2018-01-24 23:11:49 +0530

    some

--- account/register.php
+++ account/register.php
@@ -1,92 +1,11 @@
-<?php
-require "config.php";
-if(isset($_POST['signup'])){
-  if(isset($_POST['name'])      and
-    isset($_POST['token'])      and
-    isset($_POST['username'])   and
-    isset($_POST['email'])      and
-    isset($_POST['mobile'])     and
-    isset($_POST['aadhar'])){
-
-                      if($_POST['name'] == '' ||
-                        $_POST['token'] == '' ||
-                        $_POST['username'] == '' ||
-                        $_POST['email'] == '' ||
-                        $_POST['mobile'] == '' ||
-                        $_POST['aadhar'] == '' ){
-
-        }
-        else {
-                $name = $user->crtstr($_POST['name']);
-                $username = $user->crtstr($_POST['username']);
-                $email = $user->crtstr($_POST['email']);
-                $mobile = $user->crtstr($_POST['mobile']);
-                $aadhar = $user->crtstr($_POST['aadhar']);
-
-
-
-              if(!($user->registration($name,$username,$email,$mobile,$aadhar)))
-              {
-                          echo "done";
-              }
-
-
-
-        }
+<script>
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://platform.clickatell.com/messages/http/send?apiKey=quap0_ZSQeqlaXjyYYaO6A==&to=918979836671&content=Test+message+text", true);
+xhr.onreadystatechange = function(){
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log('success');
     }
-    else {
+};
+xhr.send();
 
-      }
-}
-else {
-  $user->redirectTo('account/');
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Accounts | eGov</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Kite+One|Pacifico" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
-<meta name="theme-color" content="#333">
-</head>
-<body>
-
-	<div class="foto">
-		<div class="wrap">
-			<img src="static/photos/index.png" draggable="false" >
-		</div>
-	</div>
-
-	<div class="log">
-		<center><h1><span style="font-family: 'Pacifico', cursive;font-size:35px;">e</span>GOV</h1>
-			<span><b>Power&nbsp&nbspTo&nbsp&nbspThe&nbsp&nbspPeople  </b></span></center>
-			<hr>
-
-
-
-			<div class="activitation" >
-				<form class="lg-frm" id="register" >
-				<div class="form-group">
-					<input type="text" name="otp" id="otp" placeholder="otp [check your email]" class="form-control">
-				</div>
-        <div class="form-group">
-					<input type="password" name="pass" id="pass" placeholder="Password" class="form-control">
-				</div>
-				<div class="form-group">
-					<input type="password" name="rpass" id="rpass" placeholder="Re-enter Password" class="form-control">
-				</div>
-				<button type="submit" value="submit" name="submit" >Submit</button>
-
-			</form>
-			</div>
-	</div>
-	<script src="js/validate.js" async ></script>
-  <script src="js/main.js" async ></script>
-</body>
-</html>
+</script>
