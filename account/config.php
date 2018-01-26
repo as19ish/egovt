39110f167035a0327ef6df9925994da2d15a391b
arrow <as19ish@gmail.com>
2018-01-26 12:55:16 +0530

    some

--- account/config.php
+++ account/config.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 require "config/main.class.php";
-//define('conString', 'mysql:host=sql12.freemysqlhosting.net;dbname=sql12217206');
-//define('dbUser', 'sql12217206');
-//define('dbPass', 'hnIfxlY1Er');
-define('conString', 'mysql:host=localhost;dbname=egov');
-define('dbUser', 'root');
-define('dbPass', '');
+define('conString', 'mysql:host=sql12.freemysqlhosting.net;dbname=sql12217206');
+define('dbUser', 'sql12217206');
+define('dbPass', 'hnIfxlY1Er');
+//define('conString', 'mysql:host=localhost;dbname=egov');
+//define('dbUser', 'root');
+//define('dbPass', '');
 $user = new User();
 if(!$user->dbConnect(conString, dbUser, dbPass)){
   die($user->getMsg());
